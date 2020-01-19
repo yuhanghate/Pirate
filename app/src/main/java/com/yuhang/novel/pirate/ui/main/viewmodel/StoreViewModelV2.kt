213d3d636f5e6005eb0e679ea0a646d4ae01807e
yuhang <714610354@qq.com>
2020-01-19 08:50:59 +0800

    1.增加版本号

--- app/src/main/java/com/yuhang/novel/pirate/ui/main/viewmodel/StoreViewModelV2.kt
+++ app/src/main/java/com/yuhang/novel/pirate/ui/main/viewmodel/StoreViewModelV2.kt
@@ -5,35 +5,46 @@ import com.yuhang.novel.pirate.base.BaseViewModel
 import com.yuhang.novel.pirate.ui.store.fragment.LadyFragment
 import com.yuhang.novel.pirate.ui.store.fragment.ManFragment
 import com.yuhang.novel.pirate.ui.store.fragment.SexFragment
-import java.util.ArrayList
+import java.util.*
 
 /**
  * 书城
  */
-class StoreViewModelV2:BaseViewModel() {
+class StoreViewModelV2 : BaseViewModel() {
 
     var lastTabEntity = 0
 
-//    val mTitles: ArrayList<String> = arrayListOf<String>( "男生", "女生", "小黄书")
-//    val mFragments: ArrayList<BaseFragment<*, *>> = arrayListOf(ManFragment.newInstance(), LadyFragment.newInstance(), SexFragment.newInstance())
+    val mTitles: ArrayList<String> = arrayListOf<String>("男生", "女生", "小黄书")
+    val mFragments: ArrayList<BaseFragment<*, *>> = arrayListOf(
+        ManFragment.newInstance(),
+        LadyFragment.newInstance(),
+        SexFragment.newInstance()
+    )
 
     /**
      * 获取标题
      */
-    fun getTitles():ArrayList<String> {
+    fun getTitles(): ArrayList<String> {
+        if (mTitles.isNotEmpty()) return mTitles
         val lastUser = mDataRepository.getLastUser()?.isVip
         if (lastUser == null || !lastUser) {
-            return arrayListOf<String>( "男生", "女生")
+            mTitles.addAll(arrayListOf<String>("男生", "女生"))
         }
-        return arrayListOf<String>( "男生", "女生", "小黄书")
+        mTitles.addAll(arrayListOf<String>("男生", "女生", "小黄书"))
+        return mTitles
     }
 
     fun getFragments(): ArrayList<BaseFragment<*, *>> {
+        if (mFragments.isNotEmpty()) return mFragments
         val lastUser = mDataRepository.getLastUser()?.isVip
         if (lastUser == null || !lastUser) {
-            return arrayListOf(ManFragment.newInstance(), LadyFragment.newInstance())
+            mFragments.add(ManFragment.newInstance())
+            mFragments.add(LadyFragment.newInstance())
         }
-        return arrayListOf(ManFragment.newInstance(), LadyFragment.newInstance(), SexFragment.newInstance())
+        mFragments.add(ManFragment.newInstance())
+        mFragments.add(LadyFragment.newInstance())
+        mFragments.add(SexFragment.newInstance())
+        return mFragments
     }
 
 }
